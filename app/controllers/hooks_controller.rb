6b2f10ab369219c46fcd0e32568a38ab061b2ad2
Jakub Borys <jakub.borys@gmail.com>
2014-10-31 13:28:44 +0000

    Some things about Shutl testing can definitely be improved.

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -4,26 +4,27 @@ class HooksController < ApplicationController
 
   def updateorder
     puts 'Hook from Shuttle received with params: ' + params.to_s
-    # notification = params[:notification]
-    # if notification[:type] == 'booking_update'
-    #   uri = notification[:uri]
-    #   token = notification[:shutl_booking_reference]
-    # end
-    #
-    # if token && uri
-    #   puts "Updating order for token: #{token}"
-    #   order = Order.find_by(:delivery_token => token)
-    #   if order.blank?
-    #     puts "Couldn't find order for token: #{token}"
-    #   else
-    #     puts "Order found - id = #{order.id}"
-    #     response = fetch_order_information(uri)
-    #     order.delivery_status = response
-    #     order.save
-    #   end
-    # else
-    #   puts 'Token or URI not present'
-    # end
+    notification = params[:notification]
+    token = ''
+    if notification[:type] == 'booking_update'
+      token = notification[:shutl_booking_reference]
+    end
+
+    if token.blank?
+      puts 'Token or URI not present'
+    else
+      uri = Rails.application.config.shutl_url + '/bookings/' + token
+      puts "Updating order for token: #{token}"
+      order = Order.find_by(:delivery_token => token)
+      if order.blank?
+        puts "Couldn't find order for token: #{token}"
+      else
+        puts "Order found - id = #{order.id}"
+        response = fetch_order_information(uri)
+        order.delivery_status = response
+        order.save
+      end
+    end
     render :text => 'OK'
   end
 end
