9ed4be61bc1905393394503d058d2e2197010d77
Jakub Borys <jakub.borys@gmail.com>
2014-10-31 16:58:25 +0000

    Small order display and delivery refresh updates.

--- app/controllers/Admin/delivery_controller.rb
+++ app/controllers/Admin/delivery_controller.rb
@@ -3,14 +3,25 @@ class Admin::DeliveryController < ApplicationController
 
   def update
     order = Order.find(params[:id])
-    delivery_information = fetch_order_information(Rails.application.config.shutl_url + '/bookings/' + order.delivery_token)
+    response = fetch_order_information(Rails.application.config.shutl_url + '/bookings/' + order.delivery_token)
 
-    if !delivery_information['booking'].nil? && delivery_information['booking']['reference'] == order.delivery_token
-      order.delivery_status = delivery_information
+    if !response['booking'].nil? && response['booking']['reference'] == order.delivery_token
+      order.delivery_status = response
+
+      unless response['booking']['booking_status']
+        status = response['booking']['booking_status']
+        unless status.blank?
+          new_order_status = shutl_status_to_order_status(status)
+          unless new_order_status.blank?
+            order.status_id = new_order_status
+          end
+        end
+      end
+
       order.save
     end
     respond_to do |format|
-        format.json { render json: delivery_information, status: :ok }
+        format.json { render json: response, status: :ok }
     end
   end
 end
