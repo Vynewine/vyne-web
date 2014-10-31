933e0611f6a3efe36e3afe6ea71e58fa53045d3f
Jakub Borys <jakub.borys@gmail.com>
2014-10-31 13:16:39 +0000

    Troubleshooting Sutl Hook

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -4,26 +4,26 @@ class HooksController < ApplicationController
 
   def updateorder
     puts 'Hook from Shuttle received with params: ' + params.to_s
-    notification = params[:notification]
-    if notification[:type] == 'booking_update'
-      uri = notification[:uri]
-      token = notification[:shutl_booking_reference]
-    end
-
-    if token && uri
-      puts "Updating order for token: #{token}"
-      order = Order.find_by(:delivery_token => token)
-      if order.blank?
-        puts "Couldn't find order for token: #{token}"
-      else
-        puts "Order found - id = #{order.id}"
-        response = fetch_order_information(uri)
-        order.delivery_status = response
-        order.save
-      end
-    else
-      puts 'Token or URI not present'
-    end
+    # notification = params[:notification]
+    # if notification[:type] == 'booking_update'
+    #   uri = notification[:uri]
+    #   token = notification[:shutl_booking_reference]
+    # end
+    #
+    # if token && uri
+    #   puts "Updating order for token: #{token}"
+    #   order = Order.find_by(:delivery_token => token)
+    #   if order.blank?
+    #     puts "Couldn't find order for token: #{token}"
+    #   else
+    #     puts "Order found - id = #{order.id}"
+    #     response = fetch_order_information(uri)
+    #     order.delivery_status = response
+    #     order.save
+    #   end
+    # else
+    #   puts 'Token or URI not present'
+    # end
     render :text => 'OK'
   end
 end
