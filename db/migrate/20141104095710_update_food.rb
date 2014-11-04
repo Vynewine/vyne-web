8a5bbbb62eb6175ee5329e72a45d23a00bdb73c5
Jakub Borys <jakub.borys@gmail.com>
2014-11-04 10:19:40 +0000

    Moving 'raw' food to 'preparation'

---
+++ db/migrate/20141104095710_update_food.rb
@@ -0,0 +1,9 @@
+class UpdateFood < ActiveRecord::Migration
+  def up
+    db.execute "UPDATE foods SET parent = 4 where name = 'raw';"
+  end
+
+  def db
+    ActiveRecord::Base.connection
+  end
+end
