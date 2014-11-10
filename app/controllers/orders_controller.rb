2b272d055a00d6d85d36038336487cc7716ea7b8
Jakub Borys <jakub.borys@gmail.com>
2014-11-10 18:55:40 +0000

    ETA on delivery added.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,6 +8,13 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find_by(id: params[:id], client_id: current_user)
+    if @order.status_id == 5
+      unless @order.delivery_status['booking'].blank? ||
+          @order.delivery_status['booking']['estimates'].blank? ||
+          @order.delivery_status['booking']['estimates']['eta'].blank?
+        @estimate = @order.delivery_status['booking']['estimates']['eta']
+       end
+    end
     if @order.nil?
       render :status => :forbidden, :text => 'Forbidden fruit'
     end
