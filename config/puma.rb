26337e0af56882cc53328825946fb28eceddce9c
Jakub Borys <jakub.borys@gmail.com>
2015-01-27 10:28:19 +0000

    Reverting Puma Postgis specific config.

--- config/puma.rb
+++ config/puma.rb
@@ -4,12 +4,18 @@ threads threads_count, threads_count
 
 preload_app!
 
-rackup      DefaultRackup
-port        ENV['PORT']     || 5000
+rackup DefaultRackup
+port ENV['PORT'] || 5000
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
-  ActiveRecord::Base.establish_connection
+  ActiveSupport.on_load(:active_record) do
+    config = ActiveRecord::Base.configurations[Rails.env] ||
+        Rails.application.config.database_configuration[Rails.env]
+    config['adapter'] = 'postgis'
+    config['pool'] = ENV['MAX_THREADS'] || 5
+    ActiveRecord::Base.establish_connection(config)
+  end
 
   if defined?(Resque)
     Resque.redis = Rails.application.config.redis
